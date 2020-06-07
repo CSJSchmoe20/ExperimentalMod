d548df5088c1dbeb81b6cc7c40d87a006d8b3065
CSJSchmoe20 <CSJSchmoe20@gmail.com>
2020-06-07 18:51:14 -0400

    created a simple block that drops nothing when broken

---
+++ src/main/java/com/csjschmoe20/followtutorial/init/BlockInit.java
@@ -0,0 +1,32 @@
+package com.csjschmoe20.followtutorial.init;
+
+import com.csjschmoe20.followtutorial.FollowTutorial;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
+@ObjectHolder(FollowTutorial.MOD_ID)
+@Mod.EventBusSubscriber(modid=FollowTutorial.MOD_ID, bus=Bus.MOD)
+public class BlockInit {
+    public static final Block example_block = null;
+
+    @SubscribeEvent
+    public static void registerBlocks(final RegistryEvent.Register<Block> event){
+        event.getRegistry().register(new Block(
+                Block.Properties.create(Material.IRON).hardnessAndResistance(0.5f, 15.0f).sound(SoundType.SAND)
+        ).setRegistryName("example_block"));
+    }
+
+    @SubscribeEvent
+    public static void registerBlockItems(final RegistryEvent.Register<Item> event){
+        event.getRegistry().register(new BlockItem(example_block, new Item.Properties().maxStackSize(16).group(ItemGroup.BUILDING_BLOCKS)).setRegistryName("example_block"));
+    }
+}
