2aaae420152b8a19068a311bd742debed2c4fc1f
CSJSchmoe20 <CSJSchmoe20@gmail.com>
2020-06-10 00:23:19 -0400

    added a quarry!

--- src/main/java/com/csjschmoe20/followtutorial/init/BlockInit.java
+++ src/main/java/com/csjschmoe20/followtutorial/init/BlockInit.java
@@ -2,6 +2,7 @@ package com.csjschmoe20.followtutorial.init;
 
 import com.csjschmoe20.followtutorial.FollowTutorial;
 import com.csjschmoe20.followtutorial.FollowTutorial.TutorialItemGroup;
+import com.csjschmoe20.followtutorial.objects.blocks.BlockQuarry;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
@@ -17,16 +18,22 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 @Mod.EventBusSubscriber(modid=FollowTutorial.MOD_ID, bus=Bus.MOD)
 public class BlockInit {
     public static final Block example_block = null;
+    public static final Block quarry_block = null;
 
     @SubscribeEvent
     public static void registerBlocks(final RegistryEvent.Register<Block> event){
         event.getRegistry().register(new Block(
                 Block.Properties.create(Material.IRON).hardnessAndResistance(0.5f, 15.0f).sound(SoundType.SAND)
         ).setRegistryName("example_block"));
+
+        event.getRegistry().register(new BlockQuarry().setRegistryName("quarry_block"));
+
+
     }
 
     @SubscribeEvent
     public static void registerBlockItems(final RegistryEvent.Register<Item> event){
         event.getRegistry().register(new BlockItem(example_block, new Item.Properties().maxStackSize(16).group(TutorialItemGroup.instance)).setRegistryName("example_block"));
+        event.getRegistry().register(new BlockItem(quarry_block, BlockQuarry.getItemProperties()).setRegistryName("quarry_block"));
     }
 }
